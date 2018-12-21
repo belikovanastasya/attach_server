a2a6ea1fcc39fda9c6434841e9af04873f02a0b7
andrey.yatsenko <andrey.yatsenko@codeit.com.ua>
2018-12-21 11:36:43 +0200

    create extra fields in User collection

--- models/user.model.js
+++ models/user.model.js
@@ -4,9 +4,10 @@ const Schema = mongoose.Schema;
 let UserSchema = new Schema({
     email: {type: String, required: true, max: 100, unique: true},
     password: {type: String, required: true},
-
+    firstname: {type: String, required: false},
+    secondname: {type: String, required: false},
+    about: {type: String, required: false, max: 200},
+    photo: {type: String, required: false, max: 200},
 });
-
-
 // Export the model
 module.exports = mongoose.model('User', UserSchema);
