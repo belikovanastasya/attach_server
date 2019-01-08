475d0817e654224a84e42de38329d4f7b9b5d9ba
andrey.yatsenko <andrey.yatsenko@codeit.com.ua>
2019-01-08 13:07:30 +0200

    add eslint and prettier

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -7,8 +7,8 @@ const validateLoginInput = require('../validation/validation.login');
 const config = require('../services/config');
 
 //Simple version, without validation or sanitation
-exports.test = function (req, res) {
-    res.send('Greetings from the Test controller!')
+exports.test = function(req, res) {
+  res.send("Greetings from the Test controller!");
 };
 
 exports.user_create = function(req, res) {
@@ -20,22 +20,19 @@ exports.user_create = function(req, res) {
   User.findOne({
     email: req.body.email
   }).then(user => {
-      let errors = {};
+    let errors = {};
     if (user) {
-        errors.text = "Email already exists";
-        return res.status(400).json(
-            {error: errors}
-      );
-
+      errors.text = "Email already exists";
+      return res.status(400).json({ error: errors });
     } else {
       const newUser = new User({
         email: req.body.email,
         password: req.body.password,
-        firstName: '',
-        lastName: '',
-        avatar: '',
+        firstName: "",
+        lastName: "",
+        avatar: "",
         isDesigner: false,
-        description: ''
+        description: ""
       });
 
       bcrypt.genSalt(10, (err, salt) => {
@@ -56,62 +53,62 @@ exports.user_create = function(req, res) {
   });
 };
 
-exports.user_login = function (req, res) {
-    const { error, isValid } = validateLoginInput(req.body);
-    let errors = {};
-    if (!isValid) {
-        errors.text = error.join(', ');
-        return res.status(400).json({error: errors});
+exports.user_login = function(req, res) {
+  const { error, isValid } = validateLoginInput(req.body);
+  let errors = {};
+  if (!isValid) {
+    errors.text = error.join(", ");
+    return res.status(400).json({ error: errors });
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      errors.text = "User not found";
+      return res.status(404).json({ error: errors });
     }
-
-    const email = req.body.email;
-    const password = req.body.password;
-
-    User.findOne({ email })
-        .then(user => {
-            if (!user) {
-                errors.text  = 'User not found';
-                return res.status(404).json({error: errors});
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        const payload = {
+          id: user.id,
+          name: user.name
+        };
+        jwt.sign(
+          payload,
+          config.jwtSecret,
+          {
+            expiresIn: 3600
+          },
+          (err, token) => {
+            if (err) console.error("There is some error in token", err);
+            else {
+              res.json({
+                success: true,
+                token: `Bearer ${token}`,
+                user: user
+              });
             }
-            bcrypt.compare(password, user.password)
-                .then(isMatch => {
-                    if (isMatch) {
-                        const payload = {
-                            id: user.id,
-                            name: user.name
-                        }
-                        jwt.sign(payload, config.jwtSecret, {
-                            expiresIn: 3600
-                        }, (err, token) => {
-                            if (err) console.error('There is some error in token', err);
-                            else {
-                                res.json({
-                                    success: true,
-                                    token: `Bearer ${token}`,
-                                    user: user
-                                });
-                            }
-                        });
-                    }
-                    else {
-                        errors.text  = 'Incorrect Password';
-                        return res.status(400).json({error: errors});
-                    }
-                });
-        });
-}
-exports.user_check = function (req, res) {
-    const user = jwtDecode(req.body.token);
-    const user_ID = user.id;
-    User.findOne({ _id: user_ID })
-        .then(user => {
-            if (!user) {
-                return res.status(404);
-            }
-            return res.status(200).json({user: user})
-        }
-    )
-}
+          }
+        );
+      } else {
+        errors.text = "Incorrect Password";
+        return res.status(400).json({ error: errors });
+      }
+    });
+  });
+};
+exports.user_check = function(req, res) {
+  const user = jwtDecode(req.body.token);
+  const user_ID = user.id;
+  User.findOne({ _id: user_ID }).then(user => {
+    if (!user) {
+      return res.status(404);
+    }
+    return res.status(200).json({ user: user });
+  });
+};
 // exports.user_details = function (req, res) {
 //   User.findById(req.params.id, function (err, user) {
 //       if (err) return next(err);
